285b1f7a4e80be6e9c715914396d82dcbc989612
Mardiros129 <mardiros129@gmail.com>
2025-04-10 08:20:38 +0900

    Update World.gd
    Added a quit Input capture. Need to assign to key in Godot.

--- World.gd
+++ World.gd
@@ -1,14 +1,17 @@
 extends Node2D
 
 @onready var camera = $Camera2D
-
+@export var camera_speed = 5
 
 func _unhandled_input(event):
+	if Input.is_action_pressed("Exit"):
+		get_tree().quit()
+	
 	if Input.is_action_pressed("PanUp"):
-		camera.position.y -= 10
+		camera.position.y -= camera_speed
 	if Input.is_action_pressed("PanDown"):
-		camera.position.y += 10
+		camera.position.y += camera_speed
 	if Input.is_action_pressed("PanLeft"):
-		camera.position.x -= 10
+		camera.position.x -= camera_speed
 	if Input.is_action_pressed("PanRight"):
-		camera.position.x += 10
+		camera.position.x += camera_speed
